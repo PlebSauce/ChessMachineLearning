8330d0e8455fccde16eddd256b3808dde8304127
PlebSauce <119638801+PlebSauce@users.noreply.github.com>
2024-01-19 20:51:03 -0800

    Fix main body to assign x and y and their train and test variables. No model fix yet, need to implement LSTM.

--- PythonFiles/LSTMAlgo.py
+++ PythonFiles/LSTMAlgo.py
@@ -2,46 +2,55 @@ import pandas as pd
 from matplotlib import pyplot
 import numpy as np
 from sklearn import linear_model, model_selection, metrics
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder, LabelBinarizer, LabelEncoder
 from sklearn.compose import ColumnTransformer
 
 csv_file_path = "C:/Users/jorda/PycharmProjects/ChessMachineLearning/CSV/games.csv"
 df = pd.read_csv(csv_file_path)
 columns_used = ['winner', 'white_id', 'white_rating', 'black_id', 'black_rating', 'moves']
-df_used = df[columns_used]
+df_used = df[columns_used].copy().reset_index(drop=True)
 
-onehotencoder = OneHotEncoder(sparse_output=False)
-df_encoded = pd.get_dummies(df_used, columns=['winner', 'white_id', 'black_id', 'moves'])
+df_used['player'] = df_used['white_id'].fillna(df_used['black_id'])
+df_used['player'].fillna(df_used['white_id'], inplace=True)
 
-#df_used.loc[:, ['player']] = df_used['white_id'].fillna(df_used['black_id'])
-df_used.loc[:, ['player']] = df_used['white_id'].fillna(df_used['black_id'])
-df_used.loc[:, ['player']].fillna(df_used['white_id'], inplace=True)
+df_used.drop(columns=['white_id', 'black_id'], axis=1, inplace=True)
 
-print(df_used.columns)
-print(df_encoded.columns)
-df_used.drop(['white_id', 'black_id'], axis=1, inplace=True)
-
-grouped_by_player = df_used.groupby('player')
+df_used.groupby('player')
 
 snip_length = 10
-df.loc[:, ['Sequence']] = df.groupby('player')['moves'].transform(lambda x: x.tolist())
-df.loc[:, ['NextMove']] = df.groupby('player')['moves'].shift(-1)
+df_used['moves'] = df_used['moves'].apply(lambda x: x.split())
 
-df.dropna(subset=['NextMove'], inplace=True)
+min_games = 2
+grouped_by_player = df_used.groupby('player')
+valid_players = [player for player, data in grouped_by_player if len(data) >= min_games]
+newg = df_used[df_used['player'].isin(valid_players)]
+newgrouped = newg.groupby('player')
 
-df['SnippedSequence'] = df['Sequence'].apply(lambda x: x[:snip_length])
 mylog_model = linear_model.LogisticRegression()
-
 models = {}
-for player, data in grouped_by_player:
-    X = data.drop(['player', 'NextMove'], axis=1)
-    Y = data['NextMove']
-    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.2)
 
-    player_model = linear_model.LogisticRegression()
-    player_model.fit(X_train, Y_train)
+for player, data in newgrouped:
+    data.drop(['player'], axis=1, inplace=True)
+    data['Sequence'] = data['moves'].apply(lambda x: ' '.join(x))
+    data['SnippedSequence'] = data['Sequence'].apply(lambda x: ' '.join(x.split()[:snip_length]))
+    data.drop(['Sequence', 'moves'], axis=1, inplace=True)
 
-    models[player] = player_model
+    arr = data['SnippedSequence'].values
+    data.loc[:, ['NextMove']] = arr[0].rpartition(' ')[-1]
+    data.dropna(subset=['NextMove'], inplace=True)
+
+    onehotencoder = OneHotEncoder(sparse_output=False)
+    df_encoded = pd.get_dummies(data, columns=['winner', 'SnippedSequence', 'NextMove'])
+    #le = LabelEncoder()
+    #df_encoded2 = le.fit_transform(df_used['NextMove'])
+    X = df_encoded.values[:, [0, 1, 2, 4]]
+    Y = df_encoded.values[:, 3]
+    X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.2, train_size=0.8)
+
+    #player_model = linear_model.LogisticRegression()
+    #player_model.fit(X_train, Y_train)
+
+    #models[player] = player_model
 
 player_ids_list = list(models.keys())
 print("List of Player IDs:")
