8371794323644994e6d7361f4ce749408035b91d
PlebSauce <119638801+PlebSauce@users.noreply.github.com>
2024-01-25 00:35:51 -0800

    Fix encoding issues. Add labelencoding functionality.

--- PythonFiles/LSTMAlgo.py
+++ PythonFiles/LSTMAlgo.py
@@ -2,6 +2,7 @@ import pandas as pd
 from matplotlib import pyplot
 import numpy as np
 from sklearn import linear_model, model_selection, metrics
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import OneHotEncoder, LabelBinarizer, LabelEncoder
 from sklearn.compose import ColumnTransformer
 import tensorflow as tf
@@ -32,8 +33,11 @@ newg = df_used[df_used['player'].isin(valid_players)]
 newgrouped = newg.groupby('player')
 
 mylog_model = linear_model.LogisticRegression()
+le = LabelEncoder()
 onehotencoder = OneHotEncoder(sparse_output=False)
 models = {}
+X_test_models = {}
+Y_test_models = {}
 
 for player, data in newgrouped:
     data.drop(['player'], axis=1, inplace=True)
@@ -43,41 +47,35 @@ for player, data in newgrouped:
 
     data.loc[:, ['NextMove']] = ""
     count_of_rows = 0
+    unique_moves = []
     for index, row in data.iterrows():
         arr = row['SnippedSequence']
         row['NextMove'] = arr.rpartition(' ')[-1]
         data.loc[index, 'NextMove'] = row['NextMove']
-        count_of_rows = count_of_rows + 1
+        if data.loc[index, 'NextMove'] not in unique_moves:
+            unique_moves.append(data.loc[index, 'NextMove'])
+            count_of_rows = count_of_rows + 1
     data.dropna(subset=['NextMove'], inplace=True)
+
+    data['winner'] = le.fit_transform(data['winner'] )
+    data['SnippedSequence'] = le.fit_transform(data['SnippedSequence'])
+    data['NextMove'] = le.fit_transform(data['NextMove'])
     df_encoded = pd.get_dummies(data, columns=['winner', 'SnippedSequence', 'NextMove'])
-    #le = LabelEncoder()
-    #df_encoded2 = le.fit_transform(df_used['NextMove'])
+    
+
     X = df_encoded.values[:, :(count_of_rows)]
     Y = df_encoded.values[:, :-count_of_rows]
-    print(X.shape)
-    print(Y.shape)
     
     X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.2, train_size=0.8)
 
-    print(X_train.shape)
-    print(X_test.shape)
-    print(Y_train.shape)
-    print(X_test.shape)
     onehotencoder.fit(Y_train.reshape(-1, 1))
     
     X_train_reshaped = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     X_test_reshaped = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
-    #Y_train_reshaped = np.reshape(Y_train, (Y_train.shape[0], 1))
-    #Y_test_reshaped = np.reshape(Y_test, (Y_test.shape[0], 1))
     X_train_reshaped = tf.constant(X_train_reshaped, dtype=tf.float32) 
     X_test_reshaped = tf.constant(X_test_reshaped, dtype=tf.float32)
     Y_train = tf.constant(Y_train, dtype=tf.float32)
     Y_test = tf.constant(Y_test, dtype=tf.float32)
-
-    print(X_train_reshaped.shape)
-    print(X_test_reshaped.shape)
-    print(Y_train.shape)
-    print(Y_test.shape)
 
     player_model = Sequential()
     player_model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train_reshaped.shape[1], X_train_reshaped.shape[2])))
@@ -87,18 +85,11 @@ for player, data in newgrouped:
     player_model.compile(loss='categorical_crossentropy', optimizer='adam')
     
     prediction = player_model.predict(X_test_reshaped)
-    print(prediction.shape)
-    print(X_train_reshaped.shape)
-    print(Y_train.shape)
-    print(Y_train)
 
-    print("fitting")
-    print(player_model.summary())
     player_model.fit(X_train_reshaped, Y_train, verbose=2, batch_size=1, epochs=50, validation_data=(X_test_reshaped, Y_test)) #may need validation_split=0.2
 
-    #print(metrics.accuracy_score(Y_test, prediction))
-    #print(metrics.mean_squared_error(Y_test, prediction))
-
+    X_test_models[player] = X_test_reshaped
+    Y_test_models[player] = Y_test
     models[player] = player_model
 
 player_ids_list = list(models.keys())
@@ -111,14 +102,32 @@ selected_index = int(input("Enter the index ID of the player you would like to a
 if 0 <= selected_index < len(player_ids_list):
     selected_player = player_ids_list[selected_index]
     player_model = models[selected_player]
-    prediction = player_model.predict(X_test_reshaped)
-    prediction2d = prediction.reshape(-1, 1)
+    player_x_test = X_test_models[selected_player]
+    player_y_test = Y_test_models[selected_player]
+
+    prediction = player_model.predict(player_x_test)
+    column_names = onehotencoder.get_feature_names_out()
+
+    relevant_columns = column_names[:prediction.shape[1]]
+    prediction = prediction.reshape(-1, len(relevant_columns))
     predictions = np.argmax(prediction, axis=1)
-    decoded_predictions = onehotencoder.inverse_transform(prediction2d)
+    predictions = le.inverse_transform(predictions)
+
+    decoded_predictions_index = np.argmax(prediction, axis=1)
+    decoded_predictions_label = column_names[decoded_predictions_index]
+    
+    print(column_names)                                     
+    #decoded_predictions = onehotencoder.inverse_transform(prediction)
+    print("prediction:")
     print(prediction)
-    print(decoded_predictions)
+    print("decoded_predictions:")
+    print(decoded_predictions_label)
+    print("predictions:")
     print(predictions)
-    #print(metrics.accuracy_score(Y_test, prediction))
-    #print(metrics.mean_squared_error(Y_test, prediction))
+    accuracy = accuracy_score(np.argmax(player_y_test, axis=1), predictions)
+
+    print(accuracy)
+    #print(metrics.accuracy_score(player_y_test, prediction))
+    #print(metrics.mean_squared_error(player_y_test, prediction))
 else:
     print("Invalid ID")
