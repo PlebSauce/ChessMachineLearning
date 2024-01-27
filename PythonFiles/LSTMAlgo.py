0c70fd62f6cd025ea191c9e431efc038fefe5cb3
PlebSauce <119638801+PlebSauce@users.noreply.github.com>
2024-01-27 14:32:35 -0800

    Fix accuracy testing measures. Increase sample size and games required for fine tuning.

--- PythonFiles/LSTMAlgo.py
+++ PythonFiles/LSTMAlgo.py
@@ -32,10 +32,12 @@ valid_players = [player for player, data in grouped_by_player if len(data) >= mi
 newg = df_used[df_used['player'].isin(valid_players)]
 newgrouped = newg.groupby('player')
 
-mylog_model = linear_model.LogisticRegression()
+#mylog_model = linear_model.LogisticRegression()
 le = LabelEncoder()
 onehotencoder = OneHotEncoder(sparse_output=False)
 models = {}
+X_train_models = {}
+Y_train_models = {}
 X_test_models = {}
 Y_test_models = {}
 
@@ -60,15 +62,16 @@ for player, data in newgrouped:
     data['winner'] = le.fit_transform(data['winner'] )
     data['SnippedSequence'] = le.fit_transform(data['SnippedSequence'])
     data['NextMove'] = le.fit_transform(data['NextMove'])
-    df_encoded = pd.get_dummies(data, columns=['winner', 'SnippedSequence', 'NextMove'])
+    #df_encoded = pd.get_dummies(data, columns=['winner', 'SnippedSequence', 'NextMove'])
+    df_encoded = pd.get_dummies(data, columns=['winner', 'black_rating', 'white_rating', 'SnippedSequence', 'NextMove'])
     
-
     X = df_encoded.values[:, :(count_of_rows)]
     Y = df_encoded.values[:, :-count_of_rows]
     
     X_train, X_test, Y_train, Y_test = model_selection.train_test_split(X, Y, test_size=0.2, train_size=0.8)
 
     onehotencoder.fit(Y_train.reshape(-1, 1))
+    #le.fit(Y_train.reshape(-1,1))
     
     X_train_reshaped = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
     X_test_reshaped = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
@@ -88,6 +91,8 @@ for player, data in newgrouped:
 
     player_model.fit(X_train_reshaped, Y_train, verbose=2, batch_size=1, epochs=50, validation_data=(X_test_reshaped, Y_test)) #may need validation_split=0.2
 
+    X_train_models[player] = X_train_reshaped
+    Y_train_models[player] = Y_train
     X_test_models[player] = X_test_reshaped
     Y_test_models[player] = Y_test
     models[player] = player_model
@@ -102,6 +107,8 @@ selected_index = int(input("Enter the index ID of the player you would like to a
 if 0 <= selected_index < len(player_ids_list):
     selected_player = player_ids_list[selected_index]
     player_model = models[selected_player]
+    player_x_train = X_train_models[selected_player]
+    player_y_train = Y_train_models[selected_player]
     player_x_test = X_test_models[selected_player]
     player_y_test = Y_test_models[selected_player]
 
@@ -110,24 +117,47 @@ if 0 <= selected_index < len(player_ids_list):
 
     relevant_columns = column_names[:prediction.shape[1]]
     prediction = prediction.reshape(-1, len(relevant_columns))
-    predictions = np.argmax(prediction, axis=1)
-    predictions = le.inverse_transform(predictions)
+    prediction_readable = np.argmax(prediction, axis=1)
+    prediction_readable = le.inverse_transform(prediction_readable)
 
     decoded_predictions_index = np.argmax(prediction, axis=1)
-    decoded_predictions_label = column_names[decoded_predictions_index]
-    
-    print(column_names)                                     
-    #decoded_predictions = onehotencoder.inverse_transform(prediction)
-    print("prediction:")
-    print(prediction)
-    print("decoded_predictions:")
-    print(decoded_predictions_label)
-    print("predictions:")
-    print(predictions)
-    accuracy = accuracy_score(np.argmax(player_y_test, axis=1), predictions)
+    decoded_predictions_labels = column_names[decoded_predictions_index]
 
-    print(accuracy)
-    #print(metrics.accuracy_score(player_y_test, prediction))
-    #print(metrics.mean_squared_error(player_y_test, prediction))
+    #all_labels = np.concatenate((player_y_train, player_y_test), axis=0)
+    #all_labels = np.argmax(all_labels, axis=1)
+    #le.fit(decoded_predictions_label)
+
+    #cannot convert string to float: 'x0_False;
+    #decoded_predictions_label = onehotencoder.inverse_transform(relevant_columns)
+    #y contains previously unseen labels: ['x0_True']
+    #decoded_predictions_label = le.inverse_transform(decoded_predictions_label)
+    
+    print(column_names)
+    true_labels = np.argmax(player_y_test, axis=1)
+    pred_labels = np.argmax(prediction, axis=1)
+    true_labels = le.inverse_transform(true_labels)
+    pred_labels = le.inverse_transform(pred_labels)
+    print("True Labels Shape:", true_labels.shape, "Type:", true_labels.dtype)
+    print("Predicted Labels Shape:", pred_labels.shape, "Type:", pred_labels.dtype) 
+    print("Sample True Labels:", true_labels[:10])
+    print("Sample Predicted Labels:", pred_labels[:10])
+    # Print unique values of true labels and predicted labels
+    print("Unique True Labels:", np.unique(true_labels))
+    print("Unique Predicted Labels:", np.unique(pred_labels))                                    
+    #decoded_predictions = onehotencoder.inverse_transform(prediction)
+    print("decoded_predictions_labels:")
+    print(decoded_predictions_labels)
+    print("predictions:")
+    print(prediction_readable)
+    #accuracy = accuracy_score(np.argmax(player_y_test, axis=1), prediction)
+
+    #print(accuracy)
+
+    player_y_test = np.argmax(player_y_test, axis=1)
+    prediction = np.argmax(prediction, axis=1)
+    #ValueError: `y_true` is binary while y_score is 2d with 10 classes. 
+    #If `y_true` does not contain all the labels, `labels` must be provided
+    print(metrics.top_k_accuracy_score(player_y_test, prediction))
+    print(metrics.recall_score(player_y_test, prediction))
 else:
     print("Invalid ID")
